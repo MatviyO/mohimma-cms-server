e75c3bde829d965df2abc16ef68f5acecd81f6af
Matvii Oleh <oleh.m97@gmail.com>
2025-01-28 11:38:10 +0200

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,8 @@ app.use(
     cors({
         origin: process.env.CLIENT_URL || "http://localhost:3000",
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 app.use(express.json());
